d1517bc3adf987f76b226bf445e633d5b0ca593e
Wilson Tian <wilson.tian@revvity.com>
2026-08-16 21:31:22 +0800

    feat: add FPDFText character positioning APIs for accurate text selection
    - FPDFText_GetCharBox: Get tight bounding box of a character
    - FPDFText_GetLooseCharBox: Get loose bounding box covering entire glyph
    - FPDFText_GetCharOrigin: Get origin point of a character
    - FPDFText_GetFontSize: Get font size of a character
    - FPDFText_GetFontInfo: Get font name and flags
    - FPDFText_GetCharAngle: Get rotation angle of a character
    - FPDFText_GetCharIndexAtPos: Get character index at a position
    - FPDFText_GetTextRenderMode: Get text rendering mode
    - FPDFText_GetMatrix: Get transformation matrix
    
    These APIs enable accurate text positioning for:
    - Text selection overlay in PDF editors
    - Search with highlight
    - Accessible text layers

--- extras/wasm/utils/function-names.js
+++ extras/wasm/utils/function-names.js
@@ -8,7 +8,21 @@ const inputXmlContent = fs.readFileSync(inputXmlFilename, 'utf-8');
 const inputDom = new DOMParser().parseFromString(inputXmlContent);
 const nodes = xpath.select('//member[@kind="function"]/name/text()', inputDom);
 
-const allFunctions = [...nodes.map(node => node.toString()), ...extraFunctions];
+// Additional FPDFText functions for accurate text positioning
+// These are needed for PDF editors to get character-level bounding boxes
+const additionalFunctions = [
+  'FPDFText_GetCharBox',
+  'FPDFText_GetLooseCharBox',
+  'FPDFText_GetCharOrigin',
+  'FPDFText_GetFontSize',
+  'FPDFText_GetFontInfo',
+  'FPDFText_GetCharAngle',
+  'FPDFText_GetCharIndexAtPos',
+  'FPDFText_GetTextRenderMode',
+  'FPDFText_GetMatrix',
+];
+
+const allFunctions = [...nodes.map(node => node.toString()), ...extraFunctions, ...additionalFunctions];
 
 console.log(
   JSON.stringify(allFunctions.map(functionName => '_' + functionName))
